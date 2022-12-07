05a9a3dd5ea457cf22b8156f2597f98dcd900f69
abdelhadicodi <aboude.arab12@gmail.com>
2022-12-07 11:47:40 +0200

    finish step6

---
+++ javascript-basics-01/Step_6.html+Step_6.js
@@ -0,0 +1,8 @@
+alert("please fill the form");
+function divide()
+{
+    var first_number = document.getElementById("first_number").value;
+    var second_number = document.getElementById("second_number").value;
+    var y = first_number / second_number;
+        alert("result is:  " +y);
+}
