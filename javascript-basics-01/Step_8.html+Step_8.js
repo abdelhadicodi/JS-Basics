fbcabc231d6554ed5162c4740d03cb1e8c92d0ac
abdelhadicodi <aboude.arab12@gmail.com>
2022-12-07 15:13:01 +0200

    finish step8

---
+++ javascript-basics-01/Step_8.html+Step_8.js
@@ -0,0 +1,11 @@
+function age_user(){
+    var age=document.getElementById("age").value;
+    if (age>18) {
+        alert("you are over 18");
+
+    }
+    else {
+         alert("you are under 18");
+    }
+   
+}
