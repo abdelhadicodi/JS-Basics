977ba6e985306e1e829961e99ea1460f08640f1d
abdelhadicodi <aboude.arab12@gmail.com>
2022-12-07 16:51:32 +0200

    finish step 8

--- javascript-basics-01/Step_8.html+Step_8.js
+++ javascript-basics-01/Step_8.html+Step_8.js
@@ -1,11 +1,11 @@
-function age_user(){
-    var age=document.getElementById("age").value;
-    if (age>18) {
-        alert("you are over 18");
-
+var age = prompt ("your age");
+document.getElementById("age").value = age;
+document.getElementById("validate").onclick = function(){
+    var age = document.getElementById("age").value;
+    if (age>18){
+        alert("you are over 18")
     }
-    else {
-         alert("you are under 18");
+    else{
+        alert("you are under 18")
     }
-   
 }
