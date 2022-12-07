bbcc701a52b5aa28be8735703cf2bad6c41fe5fa
abdelhadicodi <aboude.arab12@gmail.com>
2022-12-07 15:35:52 +0200

    finish step 4

--- javascript-basics-01/Step_4.html+Step_4.js
+++ javascript-basics-01/Step_4.html+Step_4.js
@@ -1,12 +1,16 @@
 var name = prompt("what is your name?");
-
+document.getElementById("name").value = name;
 var surname = prompt("what is your surname?");
+document.getElementById("surname").value = surname;
+var city = prompt("Your city?");
+document.getElementById("city").value = city;
 
-var city = prompt("where do you live");
 
-alert (
-`name: ${name}
-surname: ${surname}
-city: ${city}`
-);
-
+document.getElementById("validate").onclick = function() {
+    alert(
+       `Name: ${name}
+        Surname: ${surname}
+        City: ${city}
+        `
+    )
+};
