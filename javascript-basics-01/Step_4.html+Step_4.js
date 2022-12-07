52e251ea8d392dd94461965b007c02c507ab8213
abdelhadicodi <aboude.arab12@gmail.com>
2022-12-07 10:42:23 +0200

    finish step1

---
+++ javascript-basics-01/Step_4.html+Step_4.js
@@ -0,0 +1,9 @@
+var name = prompt("what is your name?");
+alert(`hello ${name}`);
+
+var surname = prompt("what is your surname?");
+
+var city = prompt("where do you live");
+
+alert(`i am ${name} ${surname} live in ${city}`);
+
