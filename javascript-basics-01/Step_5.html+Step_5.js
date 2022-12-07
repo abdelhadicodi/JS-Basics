302fe8d83519ee0738a7ce2a4a45c3625b523dff
abdelhadicodi <aboude.arab12@gmail.com>
2022-12-07 11:34:11 +0200

    finish step5

---
+++ javascript-basics-01/Step_5.html+Step_5.js
@@ -0,0 +1,8 @@
+alert("please fill the form");
+function multiply()
+{
+    var first_number = document.getElementById("first_number").value;
+   var  second_number = document.getElementById("second_number").value;
+    var x = first_number * second_number;
+        alert("result is:  " +x);
+}
