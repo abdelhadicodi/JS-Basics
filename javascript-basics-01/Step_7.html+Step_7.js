7db008cc7adf8d1294e90163ddb0d95c87f258f8
abdelhadicodi <aboude.arab12@gmail.com>
2022-12-07 14:56:34 +0200

    finish step7

---
+++ javascript-basics-01/Step_7.html+Step_7.js
@@ -0,0 +1,12 @@
+alert("shoes size? and your birth year?");
+function user(){
+    var shoe=document.getElementById("shoe_size").value;
+    var year=document.getElementById("year").value;
+    var a=shoe*2;
+    var b=a+5;
+    var c=b*50;
+    var d=c-year;
+    var e=d+1766;
+
+    alert("the result is "+e )
+}
