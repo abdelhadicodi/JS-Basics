b73b9192528edc78e5d44033740fcc80e42bc2e7
abdelhadicodi <aboude.arab12@gmail.com>
2022-12-09 14:18:57 +0200

    finish basic 4

--- javascript-basics-04-tdd/questions.js
+++ javascript-basics-04-tdd/questions.js
@@ -1,53 +1,63 @@
 
 const stringSize = (text) => {
+    return text.length;
 
 }
 const replaceCharacterE = (text) => {
-
+return text.replace("e", " ");
 }
 const concatString = (text1, text2) => {
 
+    return text1 + text2;
+
 }
 const showChar5 = (text) => {
+    return text.charAt(4);
 
 }
 const showChar9 = (text) => {
+    return text.slice(0, 9);
 
 }
 const toCapitals = (text) => {
+    return text.toUpperCase();
 
 }
 const toLowerCase = (text) => {
+    return text.toLowerCase();
 
 }
 const removeSpaces = (text) => {
+    return text.trim();
 
 }
 const IsString = (text) => {
+    return (typeof text) == "string";
 
 }
 
 const getExtension = (text) => {
+    return text.split(".")[1] ;
 
 }
 const countSpaces = (text) => {
-
+ return text.split(" ").length -1;
 }
 const InverseString = (text) => {
-
+return text.split("").reverse().join("");
 }
 
 const power = (x, y) => {
-
+return Math.pow(x ,y);
 }
 const absoluteValue = (num) => {
-
+return Math.abs(num);
 }
 const absoluteValueArray = (array) => {
-
+return array.map(Math.abs);
 }
 const circleSurface = (radius) => {
-
+return Math.ceil(Math.PI * Math.pow(radius, 2));
 }
 const hypothenuse = (ab, ac) => {
 
